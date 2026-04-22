22ffbb92fdc788895d5a10a149c345a34d915c3f
you-tv <135056551+you-tv@users.noreply.github.com>
2026-04-22 17:13:09 +0200

    Delete src directory

--- src/app/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { RouterProvider } from 'react-router';
-import { router } from './routes.tsx';
-
-export default function App() {
-  return <RouterProvider router={router} />;
-}
